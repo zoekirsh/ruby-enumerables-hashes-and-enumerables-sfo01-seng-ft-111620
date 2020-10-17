4abc15f6b828e04ae08fd1bf0476095d03f68b4c
Zoe Kirsh <zoekirsh@gmail.com>
2020-10-17 02:32:13 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,10 +8,11 @@
 
 def select_winner(passengers)
   winner = ""
-  passengers.each do |suite, name|
-    if suite == :suite_a && name.start_with?("A")
-      winner = name
-    end
+passengers.each do |suite, name|
+  if suite == :suite_a && name.start_with?("A")
+    winner = name
   end
 end
-winner 
+ 
+winner
+end
